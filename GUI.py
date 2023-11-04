9514692ad48e99eed0aa92d990d5616f6143abf6
Yash Pandey <ypandey474@gmail.com>
2023-11-05 02:05:55 +0530

    Fix be validator with no balance

--- GUI.py
+++ GUI.py
@@ -121,26 +121,37 @@ def main_page(p2pserver, wallet):
         if st.button("View all transactions in mempool"):
             change_screen("show_transac")
             
+        
         if st.button("Become a Validator."):
-            st.session_state.try_be_validator = True
-            current_balance = st.session_state.accounts.get_balance(st.session_state.wallet.public_key)
+
+            if st.session_state.validator:
+                st.write("You are already a validator.")
+                
+            else:
+                current_balance = st.session_state.accounts.get_balance(st.session_state.wallet.public_key)
+                
+                if current_balance < config.MIN_STAKE:
+                    st.write("You don't have enough balance to stake.")
+                    
+                else:
+                    st.session_state.try_be_validator = True
+            
+        if st.session_state.try_be_validator:
             st.write("Please enter an amount to stake.")
             st.write("Minimum Stake Required: ", config.MIN_STAKE)
             st.write("Your Current Balance: ", current_balance)
             st.session_state.numerical_value = st.number_input("Enter a numerical value", min_value = config.MIN_STAKE, max_value = current_balance, value=config.MIN_STAKE, step=1)
             
-        if st.session_state.try_be_validator:
             if st.button("Submit Stake"):
-                st.session_state.validator = True
-                #TO DO: DECREMENT BALANCE IN ACCOUNT, MAKE HIM VALIDATOR AND SEND MESSAGE
                 st.session_state.p2pserver.broadcast_new_validator(stake = st.session_state.numerical_value)
-
                 st.write("You are successfully registered as a validator.")
+                st.session_state.validator = True
                 
-                time.sleep(2)
-                
+                time.sleep(1)
                 st.session_state.try_be_validator = False
-                change_screen("main_page")
+                
+                
+                
 def initialise(private_key = None):
     if "blockchain" not in st.session_state:
         st.session_state.blockchain = Blockchain()
