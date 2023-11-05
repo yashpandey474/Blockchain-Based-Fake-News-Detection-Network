89151ee0bd963b06a81a258585ca015fd7f858b2
risingPhoenix7 <achinthyahebbar@gmail.com>
2023-11-06 01:18:58 +0530

    Verified signing in block, transactions

--- pyblock/blockchain/block.py
+++ pyblock/blockchain/block.py
@@ -5,11 +5,12 @@ import json
 # Assuming ChainUtil.py exists in the same directory
 from pyblock.chainutil import ChainUtil
 import time
-import pyblock.wallet.transaction as Transaction
+from pyblock.wallet.transaction import Transaction
 from typing import List  # Import Any if the actual type of signature is not known
 
+
 class Block:
-    def __init__(self, timestamp, lastHash, hash, transactions:List[Transaction], validator, signature):
+    def __init__(self, timestamp, lastHash, hash, transactions: List[Transaction], validator, signature):
         self.timestamp = timestamp
         self.lastHash = lastHash
         self.hash = hash
@@ -22,43 +23,48 @@ class Block:
             "timestamp": self.timestamp,
             "lastHash": self.lastHash,
             "hash": self.hash,
-            "data": self.data,
+            "transactions": [transaction.to_json() for transaction in self.transactions],
             "validator": self.validator,
             "signature": self.signature.hex() if self.signature else None
         }
-
-    def __str__(self):
-        return (f"Block -\n"
-                f"Timestamp : {self.timestamp}\n"
-                f"Last Hash : {self.lastHash}\n"
-                f"Hash      : {self.hash}\n"
-                f"Data      : {self.data}\n"
-                f"Validator : {self.validator}\n"
-                f"Signature : {self.signature}")
 
     @staticmethod
     def genesis():
         return Block("genesis time", "----", "genesis-hash", [], None, None)
 
     @staticmethod
-    def create_block(lastBlock, _data, wallet):
-        timestamp = time.time()
-        lastHash = lastBlock.hash
-        data = _data
-        hash = Block.hash(timestamp, lastHash, data)
-        validator = wallet.get_public_key()
-        signature = Block.sign_block_hash(hash, wallet)
-        return Block(timestamp, lastHash, hash, data, validator, signature)
+    def hash_transactions(transactions):
+        # Create a new list of transactions with votes set to None
+        transactions_for_hashing = [
+            {**transaction.to_json(), "votes": None} for transaction in transactions
+        ]
+        # Hash the modified list of transactions
+        return hashlib.sha256(json.dumps(transactions_for_hashing).encode('utf-8')).hexdigest()
 
     @staticmethod
-    def getHash(timestamp, lastHash, data):
+    def create_block(last_block, data, wallet):
+        timestamp = time.time()
+        last_hash = last_block.hash
+        # Create a deep copy of the data (which are transaction objects) for hashing
+        transactions = [Transaction(**tx.to_json()) for tx in data]
+        hash = Block.get_hash(timestamp, last_hash, transactions)
+        validator = wallet.get_public_key()
+        signature = Block.sign_block_hash(hash, wallet)
+        return Block(timestamp, last_hash, hash, data, validator, signature)
+
+    @staticmethod
+    def get_hash(timestamp, last_hash, transactions):
+        # Use the new helper method to hash transactions
+        transactions_hash = Block.hash_transactions(transactions)
         sha = hashlib.sha256()
-        sha.update(json.dumps(f"{timestamp}{lastHash}{data}").encode('utf-8'))
+        sha.update(
+            f"{timestamp}{last_hash}{transactions_hash}".encode('utf-8'))
         return sha.hexdigest()
 
     @staticmethod
     def block_hash(block):
-        return Block.getHash(block.timestamp, block.lastHash, block.data)
+        # Use the new helper method to hash transactions within the block
+        return Block.get_hash(block.timestamp, block.last_hash, block.transactions)
 
     @staticmethod
     def sign_block_hash(hash, wallet):
@@ -66,12 +72,14 @@ class Block:
 
     @staticmethod
     def verify_block(block):
+        # First, recreate the hash from the block's transactions and compare it to the stored hash
+        recreated_hash = Block.block_hash(block)
+        if block.hash != recreated_hash:
+            return False  # The data has been tampered with if the hashes don't match
+
+        # After confirming the hash matches, verify the signature to ensure it's from the validator
         return ChainUtil.verify_signature(
             block.validator,
             block.signature,
-            Block.block_hash(block)
+            recreated_hash  # Verify the signature against the recreated hash
         )
-
-    @staticmethod
-    def verify_leader(block, leader):
-        return block.validator == leader
--- pyblock/wallet/transaction.py
+++ pyblock/wallet/transaction.py
@@ -4,7 +4,8 @@ from pyblock.ipfs.ipfs_handler import IPFSHandler
 from pyblock.nlp.ml_model import *
 from pyblock.wallet.wallet import Wallet
 from typing import Type
-    
+
+
 class Transaction:
     def __init__(self):
         self.id = ChainUtil.id()
@@ -13,7 +14,20 @@ class Transaction:
         self.sender_reputation = None
         self.model_score = None
         self.sign = None
-        
+        self.votes = None
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "ipfs_address": self.ipfs_address,
+            "sender_address": self.sender_address,
+            "sender_reputation": self.sender_reputation,
+            "model_score": self.model_score,
+            # Assuming sign is a byte-like object that needs to be represented as a hex string
+            "sign": self.sign.hex() if self.sign else None,
+            "votes": self.votes
+        }
+
     def get_transaction_score(self):
         content = IPFSHandler.get_from_ipfs(
             self.ipfs_address
@@ -22,19 +36,22 @@ class Transaction:
         return get_score(content)
 
     @staticmethod
-    def generate_from_file(sender_wallet:Type[Wallet], file, blockchain):
+    def generate_from_file(sender_wallet: Type[Wallet], file, blockchain):
         data = file.read()
         ipfs_address = IPFSHandler.put_to_ipfs(data)
         partial_transaction = Transaction()
         partial_transaction.ipfs_address = ipfs_address
         partial_transaction.sender_address = sender_wallet.public_key
-        partial_transaction.sign = sender_wallet.sign(ChainUtil.hash(partial_transaction))
-        partial_transaction.sender_reputation = blockchain.get_balance(sender_wallet.public_key)
+
+        partial_transaction.sender_reputation = blockchain.get_balance(
+            sender_wallet.public_key)
         partial_transaction.model_score = partial_transaction.get_transaction_score()
+        partial_transaction.sign = sender_wallet.sign(
+            ChainUtil.hash(partial_transaction))
         return partial_transaction
 
     @staticmethod
-    def verify_partial_transaction(transaction, error_bound: float = 0.01):
+    def verify_transaction(transaction, error_bound: float = 0.01):
         signature = transaction.sign
         transaction.sign = None
         transaction_hash = ChainUtil.hash(transaction)
@@ -43,10 +60,9 @@ class Transaction:
         # Compare the model_score with transaction.model_score within the error bound
         if abs(model_score - transaction.model_score) > error_bound:
             return False
-        
+
         return ChainUtil.verify_signature(
             transaction.sender_address,  # Public key
             signature,  # Signature to verify
             transaction_hash  # Hash of the content that was signed
         )
-
