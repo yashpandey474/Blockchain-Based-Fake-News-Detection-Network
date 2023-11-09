0f89eaa53fc5b0d2b6c3e5552935779c057fb4a1
risingPhoenix7 <achinthyahebbar@gmail.com>
2023-11-10 01:08:35 +0530

    Fixed background

--- App/background.py
+++ App/background.py
@@ -8,7 +8,6 @@ from pyblock.blockchain.account import *
 from pyblock.blockchain.block import *
 
 
-
 # Background task that periodically checks if a block can be added
 
 
@@ -16,18 +15,15 @@ class Background:
     def __init__(self, p2pserver):
         self.time = None
         self.p2pserver = p2pserver
-        
+
     def can_add_block(block):
         return block.votes >= (0.5 * len(PEERS))
-    
+
     def run_forever(self):
         while True:
             print("BACKGROUND TASK IS RUNNING")
             current_time = int(time.time())
-            print((current_time - START_TIME.timestamp()) %
-                  BLOCK_VALIDATOR_CHOOSE_INTERVAL)
-            
-            
+
             if ((current_time - START_TIME.timestamp()) % BLOCK_VALIDATOR_CHOOSE_INTERVAL) == 0:
                 print("TRUE")
                 if self.p2pserver.received_block:
@@ -35,11 +31,12 @@ class Background:
                         self.p2pserver.blockchain.append_block(
                             st.session_state.p2pserver.received_block, self.p2pserver.transaction_pool,
                             self.p2pserver.accounts
-                    )
-                        
+                        )
+
                     self.p2pserver.received_block = None
                     self.p2pserver.block_proposer = self.p2pserver.accounts.choose_validator()
-                    print("BLOCK PROPOSER CHOSEN: ", self.p2pserver.block_proposer)
+                    print("BLOCK PROPOSER CHOSEN: ",
+                          self.p2pserver.block_proposer)
 
                 time.sleep(BLOCK_VALIDATOR_CHOOSE_INTERVAL -
-                        ((current_time - START_TIME) % BLOCK_VALIDATOR_CHOOSE_INTERVAL))
+                           ((current_time - START_TIME.timestamp()) % BLOCK_VALIDATOR_CHOOSE_INTERVAL))
