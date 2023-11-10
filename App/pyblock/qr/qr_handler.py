f19905afe07554802bd52091438533bdf83770dd
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-11-11 01:12:09 +0530

    Add option to show QR code in make_qr function

--- App/pyblock/qr/qr_handler.py
+++ App/pyblock/qr/qr_handler.py
@@ -1,11 +1,14 @@
 import segno
 from PIL import Image
 
-def make_qr(text:str):
+def make_qr(text:str, show = 0):
     qr = segno.make_qr(text)
     qr.save("qr.png",scale = 20)
-    Image.open("qr.png").show()
+
+    if show:
+        Image.open("qr.png").show()
+
     return "qr.png"
 
 if __name__ == "__main__":
-    make_qr("Hello World")
+    make_qr("Hello World", show = 1)
