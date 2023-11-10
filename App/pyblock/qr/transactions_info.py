f4bc4fb74a5bc4613c9946e735dca115e0e59a9f
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-11-11 01:12:38 +0530

    Add function to display one transactions in mempool as qr

---
+++ App/pyblock/qr/transactions_info.py
@@ -0,0 +1,19 @@
+# SHOW ALL CURRENT TRANSACTIONS IN MEMPOOL
+from datetime import datetime
+import streamlit as st
+from .qr_handler import make_qr
+
+def show_transaction(transaction):
+
+    data = f"""
+        "Model Score": {transaction.model_score}\n
+        "Sender Reputation": {transaction.sender_reputation}\n
+        "Sender Stake": {st.session_state.p2pserver.blockchain.get_stake(transaction.sender_address)}\n
+        "Transaction Fee": {transaction.fee}\n
+        "Timestamp": {datetime.fromtimestamp(transaction.timestamp).strftime("%I:%M %p on %d %B, %Y")}\n
+        "Sender Address": {transaction.sender_address}\n
+        "ID": {transaction.id}\n
+        "Content URL": "https://{transaction.ipfs_address}.ipfs.dweb.link"
+    """
+
+    return make_qr(data)
