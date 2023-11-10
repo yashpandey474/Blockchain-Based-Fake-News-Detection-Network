37b3045517c10b9d141724e9d48e09c7181edfd2
Yash Pandey <ypandey474@gmail.com>
2023-11-10 23:33:48 +0530

    Complete view sent blocks

--- App/view_sent_blocks.py
+++ App/view_sent_blocks.py
@@ -1,8 +1,35 @@
 import streamlit as st
 from change_screen import *
 from pyblock.blockchain.block import *
-
-
+import pandas as pd
+from datetime import datetime
 
 def view_sent_blocks():
-    pass
+    st.title("Blocks Broadcasted by you.")
+    blocks = st.session_state.p2pserver.accounts[
+        st.session_state.p2pserver.wallet.get_public_key()
+    ].sent_blocks
+    
+    if len(blocks) == 0:
+        st.write("You haven't broadcasted any blocks.")
+    
+    else:
+        table_data = []
+        for block in blocks:
+            table_data.append({
+                "Timestamp":  datetime.fromtimestamp(block.timestamp).strftime("%I:%M %p on %d %B, %Y"),
+                "Validator": block.validator,
+                "Number of Votes": len(block.votes),
+                "Status": ("Yet to be verified" if block not in st.session_state.p2pserver.blockchain.chain else "Added to Chain")
+            })
+        
+        st.dataframe(pd.DataFrame(table_data), height=500)
+        
+    if st.button("Back"):
+        change_screen(st.session_state.previous_screen)
+        
+        
+    
+    
+    
+    
