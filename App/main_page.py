0da96fb623f0ea319143353cdca0fa2b3e3b7f93
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-11-11 12:24:33 +0530

    gui columns

--- App/main_page.py
+++ App/main_page.py
@@ -10,35 +10,43 @@ def main_page():
     # st.title("Fake News Detection System Utilising Blockchain")
     st.write("Welcome, " + st.session_state.name)
 
-    if st.button("Upload New News"):
-        # GET UPLOADED TEXT FILE
-        st.session_state.upload_file_executed = False
-        change_screen("upload_file")
+    c1,m,c2 = st.columns([0.4,0.6,0.4])
 
-    # VIEW NEWS STORED IN BLOCKCHAIN
-    if st.button("View all Verified News"):
-        change_screen("show_blocks")
+    with c1:
 
-    if st.button("View Account Information"):
-        change_screen("account_info")
+        if st.button("Upload New News"):
+            # GET UPLOADED TEXT FILE
+            st.session_state.upload_file_executed = False
+            change_screen("upload_file")
 
-    if st.button("View Sent News/Transactions"):
-        change_screen("view_sent_news")
-        
-    if st.session_state.user_type == "Auditor":
-        if st.button("View all transactions in mempool"):
-            change_screen("show_transac")
+        # VIEW NEWS STORED IN BLOCKCHAIN
+        if st.button("View all Verified News"):
+            change_screen("show_blocks")
 
-        if not st.session_state.validator and st.button("Become a Validator"):
-            change_screen("become_validator")
+        if st.button("View Account Information"):
+            change_screen("account_info")
+
+        if st.button("View Sent News/Transactions"):
+            change_screen("view_sent_news")
             
-        if st.session_state.validator and st.button("Modify Your Stake in Network"):
-            change_screen("become_validator")
-    
-    if (st.session_state.validator
-    and st.button("View Current Block Status")):
-        change_screen("view_block_status")
+    with c2:
+        if st.session_state.user_type == "Auditor":
+            if st.button("View all transactions in mempool"):
+                change_screen("show_transac")
 
+            if not st.session_state.validator and st.button("Become a Validator"):
+                change_screen("become_validator")
+                
+            if st.session_state.validator and st.button("Modify Your Stake in Network"):
+                change_screen("become_validator")
+        
+        if (st.session_state.validator
+        and st.button("View Current Block Status")):
+            change_screen("view_block_status")
+
+    st.write("")
+    st.write("")
+    
     
     if st.button("Exit Screen"):
         change_screen("enter")
