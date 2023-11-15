e005093b76a230c2ffe9223fd30a22c6b2b705ce
risingPhoenix7 <achinthyahebbar@gmail.com>
2023-11-15 15:33:23 +0530

    working with local host. moving to jsonbin

---
+++ App/p2pfixing/tcp_functions.py
@@ -0,0 +1,143 @@
+import streamlit as st
+import requests
+import zmq
+import logging
+import threading
+import socket
+import queue
+# Improved logging
+logging.basicConfig(level=logging.INFO)
+
+# Configuration
+bin_url = "https://api.jsonbin.io/v3/b/6554946c54105e766fd047ce"
+headers = {
+    "Content-Type": "application/json",
+    "X-Master-Key": "$2a$10$egTdD5PZqyPXutDSkhJRFu7YUnEU9cSSAueMbNHNnJLv00.SnpvoS",
+    "X-Access-Key": "$2a$10$OdanrmShDr1J97Cw4TH6Eu4LL4TZY40mNdZLsHa84vu.ynzTOAKWm"
+}
+
+default_port = 59854
+default_public_key = '123'
+myaddress = 0
+# Global variables
+peers = []
+
+# Initialize session state
+if 'received_messages' not in st.session_state:
+    st.session_state['received_messages'] = []
+
+port = st.number_input("Enter the server port",
+                       min_value=1024, max_value=65535, value=default_port)
+
+
+def register(public_key, address):
+    print("Registering with public key and address")
+    data = {'public_key': public_key, 'address': address}
+    try:
+        response = requests.post(f'{server_url}/register', json=data)
+        print(f"Response from server: {response}")
+        return response.json()
+    except requests.RequestException as e:
+        logging.error(f"Registration failed: {e}")
+        return None
+
+
+def get_ip_address():
+    print("Getting IP address")
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))
+            ip_address = s.getsockname()[0]
+            print(f"Obtained IP address: {ip_address}")
+            return ip_address
+    except Exception as e:
+        logging.error(f"Error obtaining IP address: {e}")
+        return None
+
+
+def start_server(port):
+    print(f"Starting server on port {port}")
+    if port < 1024 or port > 65535:
+        st.error(
+            "Invalid port number. Please enter a port number between 1024 and 65535.")
+        return
+    ip_address = get_ip_address()
+    if ip_address is None:
+        st.error("Failed to obtain IP address. Server cannot start.")
+        return
+
+    context = zmq.Context()
+    zmq_socket = context.socket(zmq.REP)
+    zmq_socket.bind(f"tcp://{ip_address}:{port}")
+    myaddress = f"{ip_address}:{port}"
+    register(address=f"{ip_address}:{port}", public_key=default_public_key)
+
+    while True:
+        message = zmq_socket.recv_string()
+        print(f"Received message: {message}")
+        zmq_socket.send_string(
+            f"Abe teri ma ki {message}. Sent from {myaddress}")
+
+
+def broadcast_message(message):
+    print("Broadcasting message")
+    context = zmq.Context()
+    responses = []
+    newpeers = get_data()
+    print(f"Peers: {newpeers}")
+    for peer in newpeers:
+        try:
+            tcpaddr = f"tcp://{peer['address']}"
+            print(f"Sending message to {tcpaddr}")
+            zmq_socket = context.socket(zmq.REQ)
+            zmq_socket.connect(tcpaddr)
+            zmq_socket.send_string(message)
+            reply = zmq_socket.recv_string()
+            responses.append(reply)
+            print(f"Received reply from {peer['address']}: {reply}")
+        except Exception as e:
+            logging.error(f"Error communicating with {peer}: {e}")
+        finally:
+            zmq_socket.close()
+    return responses
+
+
+def get_data():
+    try:
+        response = requests.get(bin_url, headers=headers)
+        print(f"Peers Response from server: {response}")
+        return response.json()
+    except requests.RequestException as e:
+        logging.error(f"Error fetching data: {e}")
+        return None
+
+
+# Streamlit UI
+st.title('Peer-to-Peer Communication App')
+
+if st.button('Sign Up and Start Server'):
+    server_thread = threading.Thread(
+        target=start_server, args=(port,), daemon=True)
+    server_thread.start()
+    print("Server thread started")
+
+if st.button('Refresh Peers'):
+    peers = get_peers()
+    print(f"Refreshed peers: {peers}")
+
+st.header('Peers')
+if peers:
+    st.json(peers)
+else:
+    st.write('No peers available')
+
+st.header('Send a Message')
+message = st.text_area('Message')
+if st.button('Send Message'):
+    responses = broadcast_message(message)
+    print(f"Message sent, responses: {responses}")
+    if responses:
+        st.success('Message sent successfully')
+        st.write('Responses:', responses)
+    else:
+        st.error('Failed to send message')
--- App/pyblock/blockchain/block.py
+++ App/pyblock/blockchain/block.py
@@ -7,35 +7,36 @@ import numpy as np
 from pyblock.chainutil import ChainUtil
 import time
 from pyblock.wallet.transaction import Transaction
-from typing import List  # Import Any if the actual type of signature is not known
+from typing import *
+from pyblock.wallet.wallet import Wallet
 
 
 class Block:
-    def __init__(self, timestamp, lastBlock, transactions: List[Transaction], validator, index: int, signature = None):
-        #TIME OF BLOCK CREATION
+    def __init__(self, timestamp, lastBlock, transactions: List[Transaction], validator, index: int, signature=None):
+        # TIME OF BLOCK CREATION
         self.timestamp = timestamp
-        #HASH OF PREVIOUS BLOCK
-        
-        #GENESIS BLOCK
+        # HASH OF PREVIOUS BLOCK
+
+        # GENESIS BLOCK
         if index == 1:
             self.lastHash = "0000"
         else:
             self.lastHash = Block.block_hash(lastBlock)
-            
-        #LIST OF TRANSACTIONS
-        self.transactions = transactions
-        #VALIDATOR PUBLIC KEY
-        self.validator = validator
-        #SIGNATURE BY VALIDATOR
-        self.signature = signature
-        
-        #SET OF VOTES GIVEN [INITIALISE WITH JUST VALIDATOR]
-        self.votes = set()
-        #INDEX OF BLOCK IN CHAIN 
-        self.index = index
- 
 
-    #FUNCTION TO CONVERT BLOCK TO JSON
+        # LIST OF TRANSACTIONS
+        self.transactions = transactions
+        # VALIDATOR PUBLIC KEY
+        self.validator = validator
+        # SIGNATURE BY VALIDATOR
+        self.signature = signature
+
+        # SET OF VOTES GIVEN [INITIALISE WITH JUST VALIDATOR]
+        self.votes = set()
+        # INDEX OF BLOCK IN CHAIN
+        self.index = index
+
+    # FUNCTION TO CONVERT BLOCK TO JSON
+
     def to_json(self):
         return {
             "timestamp": self.timestamp,
@@ -46,61 +47,58 @@ class Block:
             "signature": self.signature.hex() if self.signature else None,
             "countofvotes": len(self.votes)
         }
-        
 
+    # CREATE THE INITIAL BLOCK
 
-    #CREATE THE INITIAL BLOCK
     @staticmethod
     def genesis():
-        return Block(timestamp = int(time.time()), 
-                     lastBlock=None, 
+        return Block(timestamp=int(time.time()),
+                     lastBlock=None,
                      transactions=[],
                      validator="Creators",
-                     signature = None, 
-                     index = 1)
+                     signature=None,
+                     index=1)
 
-    #HASH THE TRANSACTIONS IN BLOCK WITHOUT CONSIDERING THE VOTES
+    # HASH THE TRANSACTIONS IN BLOCK WITHOUT CONSIDERING THE VOTES
     @staticmethod
     def hash_transactions(transactions):
         # Create a new list of transactions with votes set to None
         transactions_for_hashing = [
             {**transaction.to_json(), "votes": None} for transaction in transactions
         ]
-        
+
         # Hash the modified list of transactions
         return hashlib.sha256(json.dumps(transactions_for_hashing).encode('utf-8')).hexdigest()
 
     @staticmethod
     def create_block(lastBlock, data, wallet, blockchain):
-        #SET THE TIMESTAMP
+        # SET THE TIMESTAMP
         timestamp = time.time()
-        #SET PREVIOUS BLOCK'S HASH
-        lastHash = Block.block_hash(lastBlock)
-        #CONVERT THE TRANSACTIOONS TO JSON 
-        transactions = [Transaction(**tx.to_json()) for tx in data]
-        #GET THE VALIDATOR'S PUBLIC KEY
+        # GET THE VALIDATOR'S PUBLIC KEY
         validator = wallet.get_public_key()
-        
-        #SIGN THE BLOCK WITH VALIDATOR'S PRIVATE KEY
+        # RETURN THE CREATED BLOCK
+        block = Block(
+            timestamp=timestamp,
+            lastBlock=lastBlock,
+            transactions=data,
+            validator=validator,
+            signature=None,
+            index=len(blockchain.chain) + 1)
+
+        # SIGN THE BLOCK
+        block.signature = Block.getBlockSignature(block, wallet)
+        return block
+
+    @staticmethod
+    def getBlockSignature(block, wallet: Type[Wallet]):
         block_data = {
-            "timestamp": timestamp,
-            "lastHash": lastHash,
-            "transactions": [transaction.to_json() for transaction in transactions],
-            "validator": validator
+            "timestamp": block.timestamp,
+            "lastHash": block.lastHash,
+            "transactions": [transaction.to_json() for transaction in block.transactions],
+            "validator": block.validator
         }
-        
         block_json = json.dumps(block_data)
-        
-        signature = wallet.sign(block_json)    
-        
-        #RETURN THE CREATED BLOCK
-        return Block(
-            timestamp = timestamp,
-            lastBlock = lastBlock,
-            transactions = data,
-            validator = validator,
-            signature = signature,
-            index=  len(blockchain.chain) + 1)
+        return wallet.sign(block_json)
 
     @staticmethod
     def get_hash(timestamp, lastHash, transactions):
@@ -124,14 +122,12 @@ class Block:
         # Use the new helper method to hash transactions within the block
         return Block.get_hash(block.timestamp, block.lastHash, block.transactions)
 
-
     @staticmethod
     def verify_block(block):
-        #VERIFY ALL THE TRANSACTIONS IN BLOCK
+        # VERIFY ALL THE TRANSACTIONS IN BLOCK
         for transaction in block.transactions:
             if not Transaction.verify_transaction(transaction=transaction):
                 return False
-            
 
         block_data = {
             "timestamp": block.timestamp,
@@ -139,8 +135,8 @@ class Block:
             "transactions": [transaction.to_json() for transaction in block.transactions],
             "validator": block.validator
         }
-        
-        #VERIFY THE SIGNATURE OF THE BLOCK
+
+        # VERIFY THE SIGNATURE OF THE BLOCK
         return ChainUtil.verify_signature(
             block.validator,
             block.signature,
