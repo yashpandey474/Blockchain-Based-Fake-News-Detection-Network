6b0334ccc533729c70cad75f4530074bb004bd9a
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-11-16 12:12:38 +0530

    check for zero transactions on block status

--- App/view_block_status.py
+++ App/view_block_status.py
@@ -27,64 +27,67 @@ def propose_block():
         #GET CURRENT MEMPOOL TRANSACTIONS
         transactions = st.session_state.p2pserver.transaction_pool.transactions
 
-        #FOR EACH TRANSACTION
-        for transaction in transactions:
-            st.subheader(f"Transaction {transaction.id}")
-            
-            #WHETHER TO INCLUDE IN BLOCK OR NOT
-            include_value = st.radio("Include in Block?", [
-                                    "False", "True"], key=f"include_{transaction.id}")
-            
-            #WHETHER NEWS IS FAKE OR TRUE
-            vote_value = st.radio("Vote on this Transaction?", [
-                                "False", "True"], key=f"vote_{transaction.id}")
-            
-            if include_value == "True":
-                selected_transactions.append(transaction)
-                votes[transaction.id] = vote_value
+        if not transactions:
+            st.warning("No transactions in the mempool.")
+        else:
+            #FOR EACH TRANSACTION
+            for transaction in transactions:
+                st.subheader(f"Transaction {transaction.id}")
+                
+                #WHETHER TO INCLUDE IN BLOCK OR NOT
+                include_value = st.radio("Include in Block?", [
+                                        "False", "True"], key=f"include_{transaction.id}")
+                
+                #WHETHER NEWS IS FAKE OR TRUE
+                vote_value = st.radio("Vote on this Transaction?", [
+                                    "False", "True"], key=f"vote_{transaction.id}")
+                
+                if include_value == "True":
+                    selected_transactions.append(transaction)
+                    votes[transaction.id] = vote_value
 
 
-        # WARN USER IF MORE THAN ALLOWED TRANSACTIONS SELECTED
-        print(selected_transactions)
-        print(len(selected_transactions))
-        max_selections = config.BLOCK_TRANSACTION_LIMIT
-        
-        if len(selected_transactions) > max_selections:
-            st.warning(
-                f"Maximum selections allowed: {max_selections}. Please deselect items.")
+            # WARN USER IF MORE THAN ALLOWED TRANSACTIONS SELECTED
+            print(selected_transactions)
+            print(len(selected_transactions))
+            max_selections = config.BLOCK_TRANSACTION_LIMIT
+            
+            if len(selected_transactions) > max_selections:
+                st.warning(
+                    f"Maximum selections allowed: {max_selections}. Please deselect items.")
 
-        # CONFIRM THE SELECTION AND VOTES
-        if st.button("Create Block") and 1 <= len(selected_transactions) <= max_selections:
-            print("BLOCK BEING CREATED")
-            for transaction in selected_transactions:
-                if votes[transaction.id] == "True":
-                    transaction.positive_votes.add(public_key)
-                else:
-                    transaction.negative_votes.add(
-                        public_key)
-            
-            # CREATE A BLOCK WITH TRANSACTIONS (PASSED AS LIST)
-            block = Block.create_block(
-                lastBlock=st.session_state.blockchain.chain[-1],
-                data=selected_transactions,
-                wallet=st.session_state.p2pserver.wallet,
-                blockchain=st.session_state.p2pserver.blockchain
-            )
-            
-            print(block.transactions)
-            
-            #ADD THE ADDRESS TO VOTE
-            block.votes.add(st.session_state.wallet.get_public_key())
+            # CONFIRM THE SELECTION AND VOTES
+            if st.button("Create Block") and 1 <= len(selected_transactions) <= max_selections:
+                print("BLOCK BEING CREATED")
+                for transaction in selected_transactions:
+                    if votes[transaction.id] == "True":
+                        transaction.positive_votes.add(public_key)
+                    else:
+                        transaction.negative_votes.add(
+                            public_key)
+                
+                # CREATE A BLOCK WITH TRANSACTIONS (PASSED AS LIST)
+                block = Block.create_block(
+                    lastBlock=st.session_state.blockchain.chain[-1],
+                    data=selected_transactions,
+                    wallet=st.session_state.p2pserver.wallet,
+                    blockchain=st.session_state.p2pserver.blockchain
+                )
+                
+                print(block.transactions)
+                
+                #ADD THE ADDRESS TO VOTE
+                block.votes.add(st.session_state.wallet.get_public_key())
 
-            # BROADCAST THE BLOCK
-            st.session_state.p2pserver.broadcast_block(block)
+                # BROADCAST THE BLOCK
+                st.session_state.p2pserver.broadcast_block(block)
 
-            # CONFIRMATION MESSAGE
-            st.success("The created block was transmitted.")
-            
-            
-            # st.session_state.created_block = True
-            # st.rerun()
+                # CONFIRMATION MESSAGE
+                st.success("The created block was transmitted.")
+                
+                
+                # st.session_state.created_block = True
+                # st.rerun()
 
     if st.button("Back"):
         change_screen("main_page")
